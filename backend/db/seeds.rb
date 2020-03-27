731516d02e78c053430bb36ca61ce51aeee9b921
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-27 16:48:23 -0400

    init creatures in seeds for now

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Creature.destroy_all
+Hemisphere.destroy_all
+CreatureScraper.scrape
+Creature.set_all_creatures_times
