bc7c6d613c6bac83300ba022f664abb3378bbcf2
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-27 16:48:04 -0400

    method to set start and end times

--- backend/app/models/creature.rb
+++ backend/app/models/creature.rb
@@ -8,4 +8,26 @@ class Creature < ApplicationRecord
   def self.all_bugs
     Creature.where(c_type: "bug")
   end
+
+  def set_times
+    times = []
+    if self.time == "All day"
+      times = [0, 23]
+    else
+      times = time.split(" - ").collect do |t|
+        hour, meridiem = t.split(" ")
+        if meridiem == "PM"
+          hour = hour.to_i + 12
+        end
+        hour.to_s
+      end
+    end
+    self.update(start_time: times.first, end_time: times.last)
+  end
+
+  def self.set_all_creatures_times
+    Creature.all.each do |c|
+      c.set_times
+    end
+  end
 end
