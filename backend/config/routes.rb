58adb3042451d776f09f1ceaa39c45d078fa1032
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-28 16:09:07 -0400

    current route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :creatures
 
   get '/fish', to: 'creatures#fish'
+  get '/current', to: 'creatures#current'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
