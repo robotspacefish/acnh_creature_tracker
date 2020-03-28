3ce6dbb981593808e81e45a4d56c2df8e8c6a630
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-28 14:59:53 -0400

    get current month

--- backend/app/models/application_record.rb
+++ backend/app/models/application_record.rb
@@ -1,3 +1,14 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  @@months = [nil, "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+    def months
+      @@months
+    end
+
+    def current_month
+      current_month = Time.now.month
+      months[current_month]
+    end
 end
