b901153d3543678a660babc21b79c6177c37893f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-28 12:19:01 -0400

    methods to get a user's fish and bugs

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   has_many :creatures_users
   has_many :creatures, through: :creatures_users
+
+  def fish
+    self.creatures.fish
+  end
+
+  def bugs
+    self.creatures.bugs
+  end
 end
