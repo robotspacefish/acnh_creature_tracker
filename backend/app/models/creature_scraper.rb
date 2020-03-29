c9413c582ca219a3ad4901549e6ac2e396a572b4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-29 17:10:15 -0400

    fix bug scraper

--- backend/app/models/creature_scraper.rb
+++ backend/app/models/creature_scraper.rb
@@ -14,10 +14,8 @@ class CreatureScraper
     bug_path = "Bugs_(New_Horizons)"
     CreatureScraper.new("#{base_path}#{fish_path}", :fish, "north").call
     CreatureScraper.new("#{base_path}#{fish_path}", :fish, "south").call
-
-    # todo - html structure is different
-    # CreatureScraper.new("#{base_path}#{bug_path}", :bug, "north").call
-    # CreatureScraper.new("#{base_path}#{bug_path}", :bug, "south").call
+    CreatureScraper.new("#{base_path}#{bug_path}", :bug, "north").call
+    CreatureScraper.new("#{base_path}#{bug_path}", :bug, "south").call
   end
 
   def call
@@ -33,21 +31,22 @@ class CreatureScraper
             creature = Creature.create(scrape_fish_row(row_doc))
         end
       end
-      creature.hemispheres.build(scrape_hemisphere(row_doc))
+      creature.hemispheres.build(scrape_hemisphere(row_doc, @type == :fish ? 6 : 5))
       creature.save
-        # TODO save check
     end
   end
 
   private
     def rows
       title = ''
+
       if @hemisphere == "north"
         title = "Northern"
       else
         title = "Southern"
       end
-      table = @doc.xpath("//div[@title='#{title} Hemisphere']").css('table.roundy.sortable')
+
+      table = @doc.xpath("//div[@title='#{title} Hemisphere']").css('table.sortable')
       @rows ||= table.css("tr").collect { |row| row.css("td") }.filter { |array| !array.empty? }
     end
 
@@ -56,7 +55,6 @@ class CreatureScraper
         c_type: :fish,
         :name => row[0].text.lstrip.gsub("\n", "").downcase,
         :url => row[0].css("a").attribute("href").value,
-        :image_url => row[1].css("img").attribute("src").value,
         :price => row[2].text.lstrip.gsub("\n", ""),
         :location => row[3].text.lstrip.gsub("\n", ""),
         :shadow_size => row[4].text.lstrip.gsub("\n", ""),
@@ -67,30 +65,29 @@ class CreatureScraper
     def scrape_bug_row(row)
       {
         c_type: :bug,
-        :name => row[0].text.lstrip.gsub("\n", ""),
+        :name => row[0].text.lstrip.gsub("\n", "").downcase,
         :url => row[0].css("a").attribute("href").value,
-        :image_url => row[1].css("img").attribute("src").value,
         :price => row[2].text.lstrip.gsub("\n", ""),
         :location => row[3].text.lstrip.gsub("\n", ""),
         :time => row[4].text.lstrip.gsub("\n", ""),
       }
     end
 
-    def scrape_hemisphere(row)
+    def scrape_hemisphere(row, start_index)
       {
         h_type: @hemisphere,
-        january: row[6].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        february: row[7].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        march: row[8].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        april: row[9].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        may: row[10].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        june: row[11].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        july: row[12].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        august: row[13].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        september: row[14].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        october: row[15].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        november: row[16].text.lstrip.gsub("\n", "") == "-" ? false : true,
-        december: row[17].text.lstrip.gsub("\n", "") == "-" ? false : true
+        january: row[start_index].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        february: row[start_index+1].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        march: row[start_index+2].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        april: row[start_index+3].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        may: row[start_index+4].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        june: row[start_index+5].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        july: row[start_index+6].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        august: row[start_index+7].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        september: row[start_index+8].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        october: row[start_index+9].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        november: row[start_index+10].text.lstrip.gsub("\n", "") == "-" ? false : true,
+        december: row[start_index+11].text.lstrip.gsub("\n", "") == "-" ? false : true
       }
 
     end
@@ -104,5 +101,3 @@ class CreatureScraper
       Hemisphere.destroy_all
     end
 end
-
-# c.all.map { |c| puts c.name }
