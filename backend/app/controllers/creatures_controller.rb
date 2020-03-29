ce93a6568e875014c34d33a89cd3f6ff01f8011d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-03-29 17:10:24 -0400

    bugs route

--- backend/app/controllers/creatures_controller.rb
+++ backend/app/controllers/creatures_controller.rb
@@ -9,6 +9,11 @@ class CreaturesController < ApplicationController
     render  json: CreatureSerializer.new(fish).to_serialized_json
   end
 
+  def bugs
+    bugs = Creature.bugs
+    render  json: CreatureSerializer.new(bugs).to_serialized_json
+  end
+
   def current
     creatures = Creature.available_now
     render json: CreatureSerializer.new(creatures).to_serialized_json
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :creatures
 
   get '/fish', to: 'creatures#fish'
+  get '/bugs', to: 'creatures#bugs'
   get '/current', to: 'creatures#current'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
