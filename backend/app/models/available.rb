116ed255d1184e4a5a66d9f5c204941a29423e0f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-08 12:39:39 -0400

    move time setting methods from creature to available

--- backend/app/models/available.rb
+++ backend/app/models/available.rb
@@ -1,4 +1,26 @@
 class Available < ApplicationRecord
   has_many :availables_creatures
   has_many :creatures, through: :availables_creatures
+
+   def set_times
+    times = []
+    if self.time == "All day"
+      times = [0, 23]
+    else
+      times = time.split(" - ").collect do |t|
+        hour, meridiem = t.split(" ")
+        if meridiem == "PM"
+          hour = hour.to_i + 12
+        end
+        hour
+      end
+    end
+    self.update(start_time: times.first, end_time: times.last)
+  end
+
+  def self.set_all_creatures_times
+    Creature.all.each do |c|
+      c.set_times
+    end
+  end
 end
