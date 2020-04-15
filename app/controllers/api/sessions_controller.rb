5967d4e6cba3af5ac10add057cc4a3b77df5d8ce
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 22:13:05 -0400

    sessions controller actions to create session & get current user

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:session][:username])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user).to_serialized_json
+    else
+      render json: {
+        error: "Invalid login credentials"
+      }
+    end
+  end
+
+  def get_current_user
+    if logged_in?
+      render json: UserSerializer.new(current_user)
+    else
+      render json: {
+        error: "No one logged in"
+      }
+    end
+  end
+end
