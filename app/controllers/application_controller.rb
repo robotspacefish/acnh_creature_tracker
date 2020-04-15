380f3f35be872518b423ed208aca28a7198fce90
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 22:13:18 -0400

    methods for current user and logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::API
   include ::ActionController::Cookies
+  # helper_method :current_user, :logged_in?
+
+  # private
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
 end
