d7270240fd359d5e2d9b00559316dd85263cf7ad
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-15 15:10:03 -0400

    signup route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
     get '/get_current_user', to: 'sessions#get_current_user'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
+    post '/signup', to: 'users#create'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
