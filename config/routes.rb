363b56cbdda31d246be52380e1a226aec907abef
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-14 22:13:31 -0400

    add routes to get current user and login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     get '/fish', to: 'creatures#fish'
     get '/bugs', to: 'creatures#bugs'
     get '/current', to: 'creatures#current'
+    get '/get_current_user', to: 'sessions#get_current_user'
+    post '/login', to: 'sessions#create'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
