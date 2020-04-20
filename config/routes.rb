58fe230f1fbaedd316ef478d14532d706f76e0cc
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-19 20:01:27 -0400

    add post & delete routes for creatures_user

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
     post '/signup', to: 'users#create'
+    post '/creatures_user', to: 'creatures_user#create'
+    delete '/creatures_user', to: 'creatures_user#destroy'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
