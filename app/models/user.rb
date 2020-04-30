a39e27cc012bfb57b8a30c3586ca78a6ce996259
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-04-29 23:54:21 -0400

    require hemisphere on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, presence: true, uniqueness: true
+  validates :username, :hemisphere, presence: true, uniqueness: true
 
   has_many :creatures_users
   has_many :creatures, through: :creatures_users
