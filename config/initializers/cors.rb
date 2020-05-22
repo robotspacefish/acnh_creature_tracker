1c4f207987bedea586290a40aded857e9b13f56a
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-05-21 20:41:42 -0400

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'hoosthere.netlify.app', 'https://robotspacefish.dev'
+    origins '*'
 
     resource '*',
       headers: :any,
