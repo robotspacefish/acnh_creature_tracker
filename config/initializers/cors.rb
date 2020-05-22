9004ed46a0ca4d5945bbffb65e2d053da2e73227
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-05-21 20:38:10 -0400

    update cors with netlify url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'https://robotspacefish.github.io', 'https://robotspacefish.dev'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'hoosthere.netlify.app', 'https://robotspacefish.dev'
 
     resource '*',
       headers: :any,
